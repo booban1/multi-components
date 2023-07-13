246f5a96b1d643af653a40a9445eb608e4c40237
Booban <simonnyongesa6@gmail.com>
2023-07-13 11:22:05 +0300

    everything

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Heading } from '@chakra-ui/react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+           <p>Step by step guide to the Everything of Nothing</p>
+           <div className="vl"></div>
+           <p style={{margin: "10px", fontSize: "30px"}}>1. Undestand that everything has all nothings</p>
+           <p style={{margin: "10px", fontSize: "1rem"}}>This means that there can be infinite nothings where everything has occupied everything</p>
+           <div className="vl"
+            style={{margin: "10px"}}
+           ></div>
+           <p style={{margin: "10px", fontSize: "30px"}}>2. Everything has nothing that is everything in its own</p>
+           <p style={{margin: "10px", fontSize: "1rem", maxWidth: "700px"}}>Wait, even if you take all the nothings out of everything, you will  have all nothings that are essentially nothing, and remain with everything, which has some little nothings</p>  
+           <div className="vl"
+            style={{margin: "10px"}}
+           ></div>
+           <p style={{margin: "10px", fontSize: "30px"}}>3. You can't put evrything in nothing</p>
+           <p style={{margin: "10px", fontSize: "1rem"}}>But you can put all all nothings in everything, and well, still affect noting in everything...</p>  
+           <p>Well</p>
+           <p>Just</p>
+           <p>Dont </p>
+           <p>be </p>
+           <div className="App-header"></div>
+      </div>
     </div>
   );
 }
